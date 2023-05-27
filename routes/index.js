a1241327cc0570b1c33c55d6ac459aea08241263
Asad Khan <asadkhan6192@gmail.com>
2023-05-27 22:31:35 +0500

    Define css variables for color palette

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,6 @@ router.get('/', function (req, res, next) {
 
 router.post("/new", (req, res, next) => {
   const { user, text } = req.body;
-  console.log(user, text);
   messages.push({ user, text, added: new Date() })
   res.redirect("/");
 })
